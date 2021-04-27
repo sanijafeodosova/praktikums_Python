cd906d269f151742ae3db88e5bd1bcc4f9b95446
sanijafeodosova <sanija.feodosova@grobinasskola.lv>
2021-04-27 10:48:48 +0300

    izpildīts uzd1

--- uzd1.py
+++ uzd1.py
@@ -4,3 +4,11 @@ riņķa līnijas rādiusu un izvada uz ekrāna (print)
 riņķa līnijas garumu un laukumu, atbilstoši noformējot atbildi.
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
+import math
+
+radiuss=float(input("Ievadi riņķa līnijas rādiusu: "))
+
+garums = 2*math.pi*radiuss
+laukums = math.pi*pow(radiuss,2)
+
+print("Riņķa līnijas garums ir",garums,", un laukums ir",laukums)
